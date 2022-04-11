981bc3826158f41f993a4daee7d147b069d38c54
Noran Diaconu <ndiaconu@empoweredbenefits.com>
2022-04-11 16:51:26 -0400

    Added loading overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { interval, Subject } from "rxjs";
-import { tap } from "rxjs/operators";
+import { take, tap } from "rxjs/operators";
+import { loadingService } from "./loading.service";
 
 @Component({
     selector: "app-root",
@@ -9,8 +10,14 @@ import { tap } from "rxjs/operators";
 })
 export class AppComponent {
     title = "rxjs-masterclass";
+    private readonly unsubscribe$ = new Subject();
 
     ngOnInit(): void {
+        this.interval();
+        this.loading2();
+    }
+
+    interval(): void {
         const observer = {
             next: (val: any) => console.log("next", val),
             error: (err: any) => console.log("error", err),
@@ -24,7 +31,44 @@ export class AppComponent {
         subject.next("World");
         // next messages will be printed twice (subscription and subscriptionTwo) every two seconds
         // new interval will only be printed once every two seconds
-        const interval$ = interval(2000).pipe(tap((value: number) => console.log("new interval", value)));
+        const interval$ = interval(2000).pipe(
+            take(5),
+            tap((value: number) => console.log("new interval", value))
+        );
         interval$.subscribe(subject);
+    }
+
+    // old way of displaying the loading overlay
+    loading(): void {
+        const loadingOverlay = document.getElementById("loading-overlay");
+        const loading$ = new Subject();
+        loading$.subscribe(isLoading => {
+            if (isLoading) {
+                loadingOverlay?.classList.add("open");
+            } else {
+                loadingOverlay?.classList.remove("open");
+            }
+        });
+        loading$.next(true);
+        setTimeout(() => loading$.next(false), 3000);
+    }
+
+    // better method of displaying the loading overlay where show/hide methods are called instead
+    loading2(): void {
+        const loadingOverlay = document.getElementById("loading-overlay");
+        loadingService.loadingStatus$.subscribe(isLoading => {
+            if (isLoading) {
+                loadingOverlay?.classList.add("open");
+            } else {
+                loadingOverlay?.classList.remove("open");
+            }
+        });
+        loadingService.showLoading();
+        setTimeout(() => loadingService.hideLoading(), 1000);
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
     }
 }
---
+++ src/app/loading.service.ts
@@ -0,0 +1,8 @@
+import { Subject } from "rxjs";
+
+const loading$ = new Subject();
+export const loadingService = {
+    showLoading: () => loading$.next(true),
+    hideLoading: () => loading$.next(false),
+    loadingStatus$: loading$.asObservable()
+}
