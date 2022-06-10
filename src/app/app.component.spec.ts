01456ae51c76ddcdb22002f299f1de1e4dba89ec
Noran Diaconu <ndiaconu@empoweredbenefits.com>
2022-06-10 16:59:23 -0400

    Added test and updated test packages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { map } from "rxjs/operators";
 import { TestScheduler } from "rxjs/testing";
 
 describe("Marble testing in RxJS", () => {
@@ -15,6 +16,16 @@ describe("Marble testing in RxJS", () => {
             const source$ = cold("--a-b---c");
             const expected =     "--a-b---c";
             expectObservable(source$).toBe(expected);
+        });
+    });
+
+    it("should allow configuration of emitted values", () => {
+        testScheduler.run(helpers => {
+            const { cold, expectObservable } = helpers;
+            const source$ = cold("--a-b---c", { a: 1, b: 2, c: 3});
+            const final$ = source$.pipe(map(val => val * 10));
+            const expected =     "--a-b---c";
+            expectObservable(final$).toBe(expected, { a: 10, b: 20, c: 30});
         });
     })
 })
