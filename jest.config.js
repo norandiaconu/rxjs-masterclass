1ffec4a6867a9568ddb01592703c582f57be81dc
Noran Diaconu <norandiaconu@gmail.com>
2024-05-29 20:05:53 -0400

    Updated to Angular 16

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     preset: "jest-preset-angular",
     setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
-    collectCoverage: true
+    collectCoverage: true,
+    silent: true,
 };
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { concat, from, interval, of, Subject } from "rxjs";
 import { catchError, delay, map, mergeMap, take, toArray } from "rxjs/operators";
 import { TestScheduler } from "rxjs/testing";
@@ -13,7 +13,7 @@ describe("Marble testing in RxJS", () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule]
+            imports: [HttpClientTestingModule],
         });
 
         fixture = TestBed.createComponent(AppComponent);
@@ -40,7 +40,7 @@ describe("Marble testing in RxJS", () => {
 
     it("should multicastInterval", () => {
         const subs = app.multicastInterval();
-        subs.forEach(sub => {
+        subs.forEach((sub) => {
             sub.unsubscribe();
             expect(sub.closed).toBeTruthy();
         });
@@ -49,7 +49,7 @@ describe("Marble testing in RxJS", () => {
     it("should behaviorSubject", () => {
         const subs = app.behaviorSubject();
         jest.runAllTimers();
-        subs.forEach(sub => {
+        subs.forEach((sub) => {
             sub.unsubscribe();
             expect(sub.closed).toBeTruthy();
         });
@@ -88,15 +88,15 @@ describe("Marble testing in RxJS", () => {
 
     it("should useObservableStore", () => {
         const store = app.useObservableStore();
-        expect(store._store.value).toStrictEqual({"isAuthenticated": true, "user": "Diaconu"});
+        expect(store._store.value).toStrictEqual({ isAuthenticated: true, user: "Diaconu" });
     });
-    
+
     it("should useReplaySubject", () => {
         const sub = app.useReplaySubject();
         sub.unsubscribe();
         expect(sub.closed).toBeTruthy();
     });
-    
+
     it("should useShareReplay", () => {
         const sub = app.useShareReplay();
         jest.runAllTimers();
@@ -111,7 +111,7 @@ describe("Marble testing in RxJS", () => {
 
     it("should useAsyncScheduler", () => {
         const subs = app.useAsyncScheduler();
-        subs.forEach(sub => {
+        subs.forEach((sub) => {
             sub.unsubscribe();
             expect(sub.closed).toBeTruthy();
         });
@@ -125,7 +125,7 @@ describe("Marble testing in RxJS", () => {
 
     it("should useAsyncSchedulerCounter", () => {
         const subs = app.useAsyncSchedulerCounter();
-        subs.forEach(sub => {
+        subs.forEach((sub) => {
             sub.unsubscribe();
             expect(sub.closed).toBeTruthy();
         });
@@ -133,7 +133,7 @@ describe("Marble testing in RxJS", () => {
 
     it("should useAsapSchedulerCounter", () => {
         const subs = app.useAsapSchedulerCounter();
-        subs.forEach(sub => {
+        subs.forEach((sub) => {
             sub.unsubscribe();
             expect(sub.closed).toBeTruthy();
         });
@@ -178,7 +178,7 @@ describe("Marble testing in RxJS", () => {
 
     it("should autoUnsubscribe", () => {
         const sub = app.autoUnsubscribe();
-        document.dispatchEvent(new MouseEvent('click'));
+        document.dispatchEvent(new MouseEvent("click"));
         jest.runAllTimers();
         sub.unsubscribe();
         expect(sub.closed).toBeTruthy();
@@ -186,7 +186,7 @@ describe("Marble testing in RxJS", () => {
 
     it("should conditionalSubscribe", () => {
         const subs = app.conditionalSubscribe();
-        subs.forEach(sub => {
+        subs.forEach((sub) => {
             sub.unsubscribe();
             expect(sub.closed).toBeTruthy();
         });
@@ -203,33 +203,33 @@ describe("Marble testing in RxJS", () => {
     });
 
     it("should convert ASCII diagrams into observables", () => {
-        testScheduler.run(helpers => {
+        testScheduler.run((helpers) => {
             const { cold, expectObservable } = helpers;
             const source$ = cold("--a-b---c");
-            const expected =     "--a-b---c";
+            const expected = "--a-b---c";
             expectObservable(source$).toBe(expected);
         });
     });
 
     it("should allow configuration of emitted values", () => {
-        testScheduler.run(helpers => {
+        testScheduler.run((helpers) => {
             const { cold, expectObservable } = helpers;
-            const source$ = cold("--a-b---c", { a: 1, b: 2, c: 3});
-            const final$ = source$.pipe(map(val => val * 10));
-            const expected =     "--a-b---c";
-            expectObservable(final$).toBe(expected, { a: 10, b: 20, c: 30});
+            const source$ = cold("--a-b---c", { a: 1, b: 2, c: 3 });
+            const final$ = source$.pipe(map((val) => val * 10));
+            const expected = "--a-b---c";
+            expectObservable(final$).toBe(expected, { a: 10, b: 20, c: 30 });
         });
     });
-    
+
     it("should let you identify subscription points", () => {
-        testScheduler.run(helpers => {
+        testScheduler.run((helpers) => {
             const { cold, expectObservable, expectSubscriptions } = helpers;
-            const source$ =            cold("-a---b-|");
-            const source2$ =           cold("-c---d-|");
+            const source$ = cold("-a---b-|");
+            const source2$ = cold("-c---d-|");
             const final$ = concat(source$, source2$);
-            const expected =                "-a---b--c---d-|";
-            const sourceOneExpectedSub =    "^------!";
-            const sourceTwoExpectedSub =    "-------^------!";
+            const expected = "-a---b--c---d-|";
+            const sourceOneExpectedSub = "^------!";
+            const sourceTwoExpectedSub = "-------^------!";
             expectObservable(final$).toBe(expected);
             expectSubscriptions(source$.subscriptions).toBe(sourceOneExpectedSub);
             expectSubscriptions(source2$.subscriptions).toBe(sourceTwoExpectedSub);
@@ -237,43 +237,43 @@ describe("Marble testing in RxJS", () => {
     });
 
     it("should let you test hot observables", () => {
-        testScheduler.run(helpers => {
+        testScheduler.run((helpers) => {
             const { hot, expectObservable } = helpers;
             const source$ = hot("--a-b-^-c");
             const final$ = source$.pipe(take(1));
-            const expected =     "--(c|)";
+            const expected = "--(c|)";
             expectObservable(final$).toBe(expected);
         });
     });
 
     it("should let you test synchronous operations", () => {
-        testScheduler.run(helpers => {
+        testScheduler.run((helpers) => {
             const { expectObservable } = helpers;
-            const source$ = from([1,2,3,4,5]);
+            const source$ = from([1, 2, 3, 4, 5]);
             const expected = "(abcde|)";
-            expectObservable(source$).toBe(expected, { a: 1, b: 2, c: 3, d: 4, e: 5});
-        })
+            expectObservable(source$).toBe(expected, { a: 1, b: 2, c: 3, d: 4, e: 5 });
+        });
     });
 
     it("should let you test asynchronous operations", () => {
-        testScheduler.run(helpers => {
+        testScheduler.run((helpers) => {
             const { expectObservable } = helpers;
-            const source$ = from([1,2,3,4,5]);
+            const source$ = from([1, 2, 3, 4, 5]);
             const final$ = source$.pipe(delay(200));
             const expected = "200ms (abcde|)";
-            expectObservable(final$).toBe(expected, { a: 1, b: 2, c: 3, d: 4, e: 5});
-        })
+            expectObservable(final$).toBe(expected, { a: 1, b: 2, c: 3, d: 4, e: 5 });
+        });
     });
 
     it("should let you test errors and error messages", () => {
         let errorTestScheduler = new TestScheduler(() => {});
-        errorTestScheduler.run(helpers => {
+        errorTestScheduler.run((helpers) => {
             const { expectObservable } = helpers;
             const source$ = of({ firstName: "Noran", lastName: "Diaconu" }, null).pipe(
-                map(output => `${output?.firstName} ${output?.lastName}`),
+                map((output) => `${output?.firstName} ${output?.lastName}`),
                 catchError(() => {
                     throw { message: "Invalid user!" };
-                })
+                }),
             );
             const expected = "(a#)";
             expectObservable(source$).toBe(expected, { a: "Noran Diaconu" }, { message: "Invalid user!" });
@@ -282,36 +282,34 @@ describe("Marble testing in RxJS", () => {
 
     it("should let you test errors and error messages with subscribe", () => {
         const source$ = of({ firstName: "Noran", lastName: "Diaconu" }, null).pipe(
-            map(output => `${output?.firstName} ${output?.lastName}`),
+            map((output) => `${output?.firstName} ${output?.lastName}`),
             catchError(() => {
                 throw { message: "Invalid user!" };
-            })
+            }),
         );
         const expected = ["Noran Diaconu", "Invalid user!"];
         let actual: string[] = [];
         source$.subscribe({
-            next: value => {
+            next: (value) => {
                 actual.push(value);
             },
-            error: error => {
+            error: (error) => {
                 actual.push(error);
                 expect(actual).toEqual(expected);
-            }
+            },
         });
     });
 
     it("should let you test snapshots of streams that do not complete", () => {
-        testScheduler.run(helpers => {
+        testScheduler.run((helpers) => {
             const { expectObservable } = helpers;
-            const source$ = interval(1000).pipe(
-                map(val => `${val + 1}sec`)
-            );
+            const source$ = interval(1000).pipe(map((val) => `${val + 1}sec`));
             const expected = "1s a 999ms b 999ms c";
             const unsubscribe = "4s !";
             expectObservable(source$, unsubscribe).toBe(expected, {
                 a: "1sec",
                 b: "2sec",
-                c: "3sec"
+                c: "3sec",
             });
         });
     });
@@ -320,12 +318,10 @@ describe("Marble testing in RxJS", () => {
 describe("subscribe & assert testing in RxJs", () => {
     it("should compare each emitted value", () => {
         const source$ = of(1, 2, 3);
-        const final$ = source$.pipe(
-            map(val => val *10)
-        );
+        const final$ = source$.pipe(map((val) => val * 10));
         const expected = [10, 20, 30];
         let index = 0;
-        final$.subscribe(val => {
+        final$.subscribe((val) => {
             expect(val).toEqual(expected[index]);
             index++;
         });
@@ -334,24 +330,26 @@ describe("subscribe & assert testing in RxJs", () => {
     it("should compare emitted values on completion with toArray", () => {
         const source$ = of(1, 2, 3);
         const final$ = source$.pipe(
-            map(val => val *10),
-            toArray()
+            map((val) => val * 10),
+            toArray(),
         );
         const expected = [10, 20, 30];
-        final$.subscribe(val => {
+        final$.subscribe((val) => {
             expect(val).toEqual(expected);
         });
     });
 
-    it("should let you test async operations with done callback", done => {
-        const source$ = of("Ready", "Set", "Go!").pipe(
-            mergeMap((message) => of(message))
-        );
+    it("should let you test async operations with done callback", (done) => {
+        const source$ = of("Ready", "Set", "Go!").pipe(mergeMap((message) => of(message)));
         const expected = ["Ready", "Set", "Go!"];
         let index = 0;
-        source$.subscribe(val => {
-            expect(val).toEqual(expected[index]);
-            index++;
-        }, null, done);
+        source$.subscribe(
+            (val) => {
+                expect(val).toEqual(expected[index]);
+                index++;
+            },
+            null,
+            done,
+        );
     });
 });
