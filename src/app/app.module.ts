603a663788078cfd12e6966a17651c166b2a74fe
Noran Diaconu <norandiaconu@gmail.com>
2025-04-23 20:24:31 -0400

    Updated to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { TypeaheadComponent } from './typeahead/typeahead.component';
+import { TypeaheadComponent } from "./typeahead/typeahead.component";
 
 @NgModule({
     declarations: [AppComponent, TypeaheadComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-    providers: [HttpClient, HttpClientModule],
     bootstrap: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class AppModule {}
